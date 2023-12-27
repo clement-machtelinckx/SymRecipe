4c1a1dd5ccae7fd75d93c7e9f3a15f7102c2a9aa
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2023-12-27 09:53:47 +0100

    debug du registery User

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,13 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+    /**
+     * this controller allow us to login
+     *
+     * @param AuthenticationUtils $authentification
+     * @return Response
+     */
     #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authentification): Response
     {
@@ -24,20 +31,35 @@ class SecurityController extends AbstractController
         ]);
     }
 
+
+    /**
+     * this controller allow us to logout
+     *
+     * @return void
+     */
     #[Route('/deconnexion', name:'security.logout', methods: ['GET'])]
     public function logout()
     {
         //nothing to do here
     }
 
+
+    /**
+     * this conttroller is used to register a new user
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
     #[Route('/inscription', name: 'security.registration', methods: ['GET', 'POST'])]
     public function registration(Request $request, EntityManagerInterface $manager) : Response
     {
         $user = new User();
+        $user->setRoles(['ROLE_USER']);
         $form = $this->createForm(RegistrationType::class, $user);
 
         $form->handleRequest($request);
-
+        // dd($form->getData());
         if ($form->isSubmitted() && $form->isValid()) {
 
             $user = $form->getData();
@@ -49,6 +71,7 @@ class SecurityController extends AbstractController
             $manager->flush();
 
             $this->addFlash('success', 'Votre compte a bien été créé');
+
             return $this->redirectToRoute('security.login');
         }
 
