ff40b7c658481adc3297507b2db26fb6bc628286
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2024-01-16 15:48:01 +0100

    :bug: bug correction on bootstrap style

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -56,6 +56,7 @@ class SecurityController extends AbstractController
     {
         $user = new User();
         $user->setRoles(['ROLE_USER']);
+
         $form = $this->createForm(RegistrationType::class, $user);
 
         $form->handleRequest($request);
